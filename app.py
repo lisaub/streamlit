011759afb9029e101fa612284e956a239785057e
lisaub <57632065+lisaub@users.noreply.github.com>
2024-03-20 18:21:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def principal():
     genero = st.radio("Selecciona Género", ("Masculino", "Femenino"))
     edad = st.slider("Selecciona Edad", 1, 100, 18)
     estilo_viaje = st.selectbox("Selecciona Estilo de Viaje", ("Explorar", "Aventura", "Relax", "Bienestar"))
-    pais_destino = st.text_input("País de Destino")
+    pais_destino = st.selectbox("País de Destino", options=list(info_paises.keys()))
     ciudad_destino = st.text_input("Ciudad de Destino")
     st.markdown("*Por favor, ingresa tu país y ciudad de destino. Esta información nos ayudará a proporcionar sugerencias más personalizadas.*")
     fecha_viaje = st.date_input("Selecciona Fecha de Viaje")
